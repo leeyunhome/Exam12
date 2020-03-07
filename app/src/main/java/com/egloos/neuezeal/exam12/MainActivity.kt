e4b935ededca76f912ea3eecf409c55515efb0e3
leeyunhome <leeyunhome@daum.net>
2020-03-07 12:43:15 +0900

    Initial commit

---
+++ app/src/main/java/com/egloos/neuezeal/exam12/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.egloos.neuezeal.exam12
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        userInputText.addTextChangedListener(object:TextWatcher{
+            override fun afterTextChanged(s: Editable?) {
+                previewText.text = s
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+
+        })
+    }
+}
